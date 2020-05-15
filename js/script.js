81a0b7cd2d9b78eda65b1c59ad03378bcfdcfd8b
RoBlockHead <student5@daniellpedia.com>
2020-05-15 00:39:09 -0400

    End Of Day March 14

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* Open */
+function openNav() {
+  document.getElementById("mobileNav").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+  document.getElementById("mobileNav").style.height = "0%";
+}
