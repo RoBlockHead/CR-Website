58f12fc72b50c7c51e862aded8902214661d8603
RoBlockHead <student5@daniellpedia.com>
2020-05-15 22:47:09 -0400

    Add bootstrap. Add mobile navbar for responsive design.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
-/* Open */
-function openNav() {
-  document.getElementById("mobileNav").style.height = "100%";
-}
-
-/* Close */
-function closeNav() {
-  document.getElementById("mobileNav").style.height = "0%";
-}
+$(document).ready(function(){
+			toggleFolder = function (folderId) {
+				var items = $('#' + folderId + '> .folder-items');
+				if($(items[0]).hasClass("hidden")){
+					$(items[0]).addClass("visible").removeClass("hidden");
+				} 
+				else {
+					$(items[0]).addClass("hidden").removeClass("visible");
+				}
+			}
+			toggleNav = function (x) {
+				x.classList.toggle("change");
+				$('.mNav-inner').toggleClass('visible');
+			}
+		});
